89df43231e72cff55e0210b647e972d202e5b268
Carlos Armenta <carmenta689@gmail.com>
2024-05-06 17:42:31 -0700

    Crear cuenta pantalla

---
+++ app/src/main/java/com/example/attendium/CrearCuenta.kt
@@ -0,0 +1,13 @@
+package com.example.attendium
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearCuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_cuenta)
+
+
+    }
+}
